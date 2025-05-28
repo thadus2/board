461d284f9e114e88f2c69db26dccd31a86689000
thadus2 <seunghoon3753@gmail.com>
2025-05-28 20:23:25 +0900

    fix css

---
+++ board-back/src/main/java/com/thadus/board_back/dto/response/board/PutCommentFavoriteResponseDto.java
@@ -0,0 +1,35 @@
+package com.thadus.board_back.dto.response.board;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.thadus.board_back.common.ResponseCode;
+import com.thadus.board_back.common.ResponseMessage;
+import com.thadus.board_back.dto.response.ResponseDto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PutCommentFavoriteResponseDto extends ResponseDto {
+
+    private PutCommentFavoriteResponseDto() {
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+    }
+
+    public static ResponseEntity<PutFavoriteResponseDto> success() {
+        PutCommentFavoriteResponseDto result = new PutCommentFavoriteResponseDto();
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> noExistComment() {
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXISTED_COMMENT, ResponseMessage.NOT_EXISTED_COMMENT);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> noExistUser() {
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXISTED_USER, ResponseMessage.NOT_EXISTED_USER);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(result);
+    }
+}
